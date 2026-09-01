f84e616986ae4aa6774b287ed6512fc259c93a3e
Fletchlock <fletchlockgaming@gmail.com>
2026-09-01 13:22:05 -0600

    Load first playable level through the LevelManager

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const STARTING_LEVEL := "res://scenes/levels/TestLevel.tscn"
+const STARTING_LEVEL := "res://scenes/third_person_level.tscn"
 
 func _ready() -> void:
 	LevelManager.load_level(STARTING_LEVEL)
