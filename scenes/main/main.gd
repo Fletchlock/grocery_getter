52a16f5cd647221707565572d97198e8b3718f6a
Fletchlock <fletchlockgaming@gmail.com>
2026-09-01 13:14:51 -0600

    Refactor starting level path

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,4 +1,6 @@
 extends Node
 
+const STARTING_LEVEL := "res://scenes/levels/TestLevel.tscn"
+
 func _ready() -> void:
-	LevelManager.load_level("res://scenes/levels/TestLevel.tscn")
+	LevelManager.load_level(STARTING_LEVEL)
